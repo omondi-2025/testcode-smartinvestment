b86c9abdbbc0377b175511c212e34d7fbd30b0ca
testcode-2223 <omondoclinton0@gmail.com>
2025-07-19 08:08:06 +0300

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 
 const express = require("express");
 const mongoose = require("mongoose");
-const cors = require("cors");
+const cors = ("cors");
 const path = require("path");
 
 const app = express();
